23951b4c223f5bebaf780e7d3bdd2839ac94e047
Samuel Boamah <skboamah16@gmail.com>
2023-07-28 09:53:06 +0000

    stats

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,64 +1,39 @@
 #!/usr/bin/python3
 """
-Module 0-stats
-script that reads stdin line by line and computes metrics
+Log parsing
 """
+
 import sys
-import re
-
-total_file_size = 0
-
-# sample_line = '36.196.190.72 - [2022-04-26 07:59:21.687812] '\
-#        '"GET /projects/260 HTTP/1.1" 500 878'
-
-pattern = r'^([\d]{1,3}\.){3}([\d]{1,3})'
-pattern += r'( - )(\[[\d]{4}-[\d]{2}-[\d]{2}'
-pattern += r' [\d]{2}:[\d]{2}:[\d]{2}\.[\d]{1,}\])'
-pattern += r'( "GET \/projects\/260 HTTP\/1\.1") '
-pattern += r'([\d]{3}) ([\d]{1,4})$'
-
-
-codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-
-no_of_lines = 0
-
-
-def is_line_valid(line: str) -> bool:
-    result = re.match(pattern, line)
-    if result:
-        return True
-    return False
-
-
-def generate_statistics(line: str) -> None:
-    global total_file_size
-    chars = line.split(' ')
-
-    file_size = int(chars[-1].replace('\n', ''))
-    try:
-        status_code = int(chars[-2])
-        if status_code in codes:
-            codes[status_code] += 1
-    except ValueError:
-        pass
-
-    total_file_size += file_size
-
-
-def print_statistics() -> None:
-    print('File size: {}'.format(total_file_size))
-    for key, value in sorted(codes.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))
 
 if __name__ == '__main__':
+
+    filesize, count = 0, 0
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    stats = {k: 0 for k in codes}
+
+    def print_stats(stats: dict, file_size: int) -> None:
+        print("File size: {:d}".format(filesize))
+        for k, v in sorted(stats.items()):
+            if v:
+                print("{}: {}".format(k, v))
+
     try:
-        for i, line in enumerate(sys.stdin, 1):
-            # generate statistics only for a valid log
-            if is_line_valid(line):
-                generate_statistics(line)
-            if not i % 10:
-                print_statistics()
+        for line in sys.stdin:
+            count += 1
+            data = line.split()
+            try:
+                status_code = data[-2]
+                if status_code in stats:
+                    stats[status_code] += 1
+            except BaseException:
+                pass
+            try:
+                filesize += int(data[-1])
+            except BaseException:
+                pass
+            if count % 10 == 0:
+                print_stats(stats, filesize)
+        print_stats(stats, filesize)
     except KeyboardInterrupt:
-        print_statistics()
+        print_stats(stats, filesize)
         raise
