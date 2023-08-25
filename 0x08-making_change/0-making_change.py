35ad83cdddd821f6e3566260704465af74235e70
Samuel Boamah <skboamah16@gmail.com>
2023-08-25 13:30:27 +0000

    update

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,30 +1,18 @@
 #!/usr/bin/python3
 """
-Module 0-making_change
+Change comes from within
 """
 
 
 def makeChange(coins, total):
     """
     Given a pile of coins of different values,
-    determine the fewest number of coins needed
-    to meet a given amount total.
-
-    Dyanimc Programmming Bottom Up Solution
+    determine the fewest number of coins needed to meet a given amount total
     """
     if total <= 0:
         return 0
-    # initialize list with maximum amount(which cannot be reached)
-    # this helps to calculate a new minimum
-    # e.g if total = 2, list = [3,3,3]
-    dp = [total + 1] * (total + 1)
-    # set minimum number of coins for amount 0
-    dp[0] = 0
-
-    # start from 1 because we know dp[0]
-    for amount in range(1, total + 1):
-        # test for every coin
-        for coin in coins:
-            if amount - coin >= 0:
-                dp[amount] = min(dp[amount], 1 + dp[amount - coin])
-    return dp[total] if dp[total] != total + 1 else -1
+    dp = [0] + [float("inf")] * (total)
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+    return dp[-1] if dp[-1] != float("inf") else -1
